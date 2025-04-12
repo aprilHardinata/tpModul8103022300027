c50a2c940c017b5feb0f9a16ffd709fa90f5f8b4
aprilHardinata <aprilhardihardi439@gmail.com>
2025-04-12 20:20:42 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tpModul8103022300027/CovidConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+public class CovidConfig
+{
+    public string satuan_suhu { get; set; } = "celcius";
+    public int batas_demam { get; set; } = 14;
+    public string pesan_ditolak { get; set; } = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+    public string pesan_diterima { get; set; } = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+
+    public void UbahSatuan()
+    {
+        if (satuan_suhu == "celcius")
+        {
+            satuan_suhu = "fahrenheit";
+            batas_demam = 99;
+        }
+        else
+        {
+            satuan_suhu = "celcius";
+            batas_demam = 37;
+        }
+    }
+}
+
+
--- tpModul8103022300027/Program.cs
+++ tpModul8103022300027/Program.cs
@@ -1,2 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Text.Json;
+
+CovidConfig config;
+
+if (File.Exists("covid_config.json"))
+{
+    string json = File.ReadAllText("covid_config.json");
+    config = JsonSerializer.Deserialize<CovidConfig>(json);
+}
+else
+{
+    config = new CovidConfig(); // default value
+}
+
+Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {config.satuan_suhu}:");
+double suhu = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Berapa hari yang lalu (terakhir) anda memiliki gejala demam?");
+int hari = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Apakah Anda ingin mengubah satuan suhu? (y/n)");
+string jawaban = Console.ReadLine().ToLower();
+
+if (jawaban == "y")
+{
+    config.UbahSatuan();
+    Console.WriteLine("Satuan suhu telah diubah menjadi " + config.satuan_suhu);
+}
+
+Console.WriteLine("Satuan suhu telah diubah menjadi " + config.satuan_suhu);
+
+if (suhu < config.batas_demam && hari < config.batas_demam)
+{
+    Console.WriteLine(config.pesan_diterima);
+}
+else
+{
+    Console.WriteLine(config.pesan_ditolak);
+}
+
